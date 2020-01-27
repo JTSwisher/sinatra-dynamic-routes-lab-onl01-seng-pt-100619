cb84483ea29f72e3fdca560ac312f2559bad7de0
Jeff Swisher <jefftswisher@gmail.com>
2020-01-27 20:13:59 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ class App < Sinatra::Base
 #Create a dynamic route at get '/say/:word1/:word2/:word3/:word4/:word5' that accepts five words and returns a string #containing all five words 
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do 
-    
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end 
 
 
